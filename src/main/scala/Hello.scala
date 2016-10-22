54dc6e9ed12bddcd2dade98f643fa3e5185fcf4c
Sebastian Nozzi <sebnozzi@gmail.com>
2016-10-22 09:19:06 +0200

    Formatting

--- src/main/scala/Hello.scala
+++ src/main/scala/Hello.scala
@@ -1,10 +1,10 @@
 
 object Hello {
 
-  def main(args: Array[String]):Unit = {
+  def main(args: Array[String]): Unit = {
     println("Hello, world!")
   }
 
-  def sum(a:Int, b:Int) = a + b
+  def sum(a: Int, b: Int) = a + b
 
 }
